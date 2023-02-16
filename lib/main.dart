681cf5fc43510e7fd6493ac337db13963f6de150
thecvcoder <thecvcoder@foxmail.com>
2023-02-16 17:45:49 +0800

    add wechat_assets_picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(DevicePreview(
+    enabled: !kReleaseMode,
     builder: (context) => const MyApp(),
   ));
 }
