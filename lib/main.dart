84b033c5c38db54a4c97a8acb003f85a96387a39
thecvcoder <thecvcoder@foxmail.com>
2023-02-16 16:58:21 +0800

    add device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(DevicePreview(
+    builder: (context) => const MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
